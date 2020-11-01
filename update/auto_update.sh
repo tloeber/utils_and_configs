1439f354de04b3775e0cab006a2c376eeb609b11
tloeber <thomas.loeber73@gmail.com>
2020-10-31 20:46:10 -0400

    Add more formatting to logging.

--- update/auto_update.sh
+++ update/auto_update.sh
@@ -4,7 +4,13 @@ LOG_PATH=/var/log/my_programs/auto-update/auto-update.log
 
 {
    echo $(date)
+   echo ============
+
    apt-get update
    apt-get upgrade -y
    apt-get autoclean
+
+   # Add blank line before next entry
+   echo ""
+
 } >> $LOG_PATH 2>&1
