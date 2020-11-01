165ccf2ad27233e15c3aee0195375f6e5f281532
tloeber <thomas.loeber73@gmail.com>
2020-11-01 15:23:59 -0500

    Further improvements to formatting of logs.

--- update/auto_update.sh
+++ update/auto_update.sh
@@ -3,14 +3,15 @@
 LOG_PATH=/var/log/my_programs/auto-update/auto-update.log
 
 {
-   echo $(date)
-   echo ============
+   # Visual separation from previous log entry
+   echo ""
+   echo =====================================================================
+   echo ""
+
+   echo $(date) - ${BASH_SOURCE[0]}
 
    apt-get update
    apt-get upgrade -y
    apt-get autoclean
-
-   # Add blank line before next entry
-   echo ""
 
 } >> $LOG_PATH 2>&1
