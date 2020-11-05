9c8c0c86d816114c6753125d55382f9283679159
tloeber <thomas.loeber73@gmail.com>
2020-11-04 21:18:52 -0500

    Add backup for installed packages, partition table, and MBR.

--- backup/s3_backup_sync.sh
+++ backup/s3_backup_sync.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Directories to back up
-backup_origins=("/etc/" "~/")
 # Keep name of S3 bucket out of version control (just in case)
 destination_bucket_path=$(cat config/backup_destination.txt)
 # Explicitly specify user's home path, since Anacron runs script as root
 home_path="/home/thomas"
 log_path="/var/log/my_programs/backups/s3_backup_sync.log"
+temp_local_backups_path="/var/tmp/ubuntu_other/"
+# Directories to back up
+backup_origins=("/etc/" "~/" "${temp_local_backup_path}/")
 aws_profile="b"
 
 {
@@ -16,10 +17,20 @@ aws_profile="b"
 	echo ""
 	echo "started at $(date) - ${BASH_SOURCE[0]}" 
 	echo "Backing up whole directories to Standard IA:"
+
+	## Generate temporary local backups
+	# Installed packages
+	dpkg --get-selections > "${temp_local_backups_path}/packages.bak"
+	# Partition table
+	fdisk -l > fdisk.bak
+	# MBR
+	dd if=/dev/sda of=MBR.bak bs=512 count=1
+
+
 	for dir in "${backup_origins[@]}"; do
 		# Add parent directory of original file to S3 as prefix.
 		# Using AWS managed KMS key, so we don't have to provide a key ID.
-		# Exclude secrets and programs installed in home directory
+		# Exclude programs installed in home directory and secrets.
 		aws s3 sync "$dir" "s3://${destination_bucket_path}${dir}" \
 			--storage-class=STANDARD_IA \
 			--profile=$aws_profile \
