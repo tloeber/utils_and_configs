5c03e51d69238b23ec0b5e37aa3110c32bc5e564
tloeber <thomas.loeber73@gmail.com>
2020-11-24 20:17:19 -0500

    Separate log file from last run from logs of multiple previous runs. Also adds additional logging messages.

--- update/auto_update.sh
+++ update/auto_update.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-LOG_PATH=/var/log/my_programs/auto-update/auto-update.log
+LOG_PATH=/var/log/my_programs/update/auto_update.log
+LOG_PATH_PREVIOUS=/var/log/my_programs/update/auto_update_previous.log
 
 {
+   # Add logs from last run to log fille with all previous runs.
+   cat $LOG_PATH >> $LOG_PATH_PREVIOUS
+
    # Visual separation from previous log entry
    echo ""
    echo =====================================================================
    echo ""
-
    echo $(date) - ${BASH_SOURCE[0]}
-
+   echo ""
+   # Add last log to previous logs
+   cat $LOG_PATH >> $LOG_PATH_PREVIOUS
+   
+   echo "UPDATING:"
    apt-get update
+   echo ""
+   
+   echo "UPGRADING:"
    apt-get upgrade -y
+   echo ""
+
+   echo "RUNNING AUTO-CLEAN:"
    apt-get autoclean
 
-} >> $LOG_PATH 2>&1
+} > $LOG_PATH 2>&1
