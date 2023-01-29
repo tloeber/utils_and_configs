67a98d0c257886963016bd705fe2f1f6535951c2
tloeber <thomas.loeber73@gmail.com>
2023-01-29 16:15:56 -0500

    Add/expand set up for linux; update docker aliases.

---
+++ dotfiles/adopt_dotfiles.sh
@@ -0,0 +1,12 @@
+
+# Backup existing dotfiles
+mv ~/.profile ~/.profile.bak
+mv ~/.bash_profile ~/.bash_profile.bak
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.bash_aliases ~/.bash_aliases.bak || echo "No existing bash_aliases found, skipping backup"
+
+# Hard-link dotfiles from repo
+ln profile ~/.profile
+ln bash_profile ~/.bash_profile
+ln bashrc ~/.bashrc
+ln bash_aliases ~/.bash_aliases
