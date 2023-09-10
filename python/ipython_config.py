856d672a3ac490dbc6c14c013397ca73c801d8a9
tloeber <thomas.loeber73@gmail.com>
2023-09-10 17:08:41 -0400

    Refine nb_mypy config

--- python/ipython_config.py
+++ python/ipython_config.py
@@ -1,4 +1,7 @@
 # Put this file in ~/.ipython/profile_default/
 
+# Activate MyPy for notebooks. Deactivate by notebook using `nb_mypy Off`
 c.InteractiveShellApp.extensions = ["nb_mypy"]
-c.InteractiveShellApp.exec_lines = ["%nb_mypy mypy-options --strict"]
+
+# Use strict mode
+# c.InteractiveShellApp.exec_lines = ["%nb_mypy mypy-options --strict"]
