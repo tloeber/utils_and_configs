51132cbdc2c3a687b2f405981373ff00a326e1a9
tloeber <thomas.loeber73@gmail.com>
2023-10-27 08:34:44 -0400

    Add pydantic plugin for  mypy; add script for starting ssh agent

--- windows/start_git_bash_with_ssh.sh
+++ windows/start_git_bash_with_ssh.sh
@@ -1,6 +1,6 @@
 # Start SSH agent and add key with non-default name
 eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_personal
 
 # Change to directory where repositories are stored
 cd /f/projects/
