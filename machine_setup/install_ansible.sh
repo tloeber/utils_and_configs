f3e08c70f5b95906f43e879144c080a4ebb07117
tloeber <thomas.loeber73@gmail.com>
2023-11-11 08:51:52 -0500

    Add ASDF for Python version management.

--- machine_setup/install_ansible.sh
+++ machine_setup/install_ansible.sh
@@ -1,4 +1,4 @@
 sudo apt update
-sudo apt install -y software-properties-common
+sudo apt install -y software-properties-common python3 python3-pip
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible python3-pip
+sudo apt install -y ansible
