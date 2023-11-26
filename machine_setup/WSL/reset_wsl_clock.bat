30875532bf7feed8c8f4c5c3612bd0cbc0a0b7ca
tloeber <thomas.loeber73@gmail.com>
2023-11-26 10:10:13 -0500

    Fix issue with batch file.

--- machine_setup/WSL/reset_wsl_clock.bat
+++ machine_setup/WSL/reset_wsl_clock.bat
@@ -1,6 +1,7 @@
-# Trigger this to run on workstation unlock (in Windows Task Scheduler)
-# Make sure to target the right WSL distro (Run `wsl --list` first)
+%REM Trigger this to run on workstation unlock (in Windows Task Scheduler)
+%REM Make sure to target the right WSL distro (Run `wsl --list` first)
 
 wsl --distribution Ubuntu-22.04 ^
     --user root ^
     --exec sudo hwclock -s
+
