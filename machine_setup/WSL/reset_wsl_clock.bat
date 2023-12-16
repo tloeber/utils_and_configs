4c3f6c47446d1fc603e53e8740549a030edbb07f
tloeber <thomas.loeber73@gmail.com>
2023-12-16 10:07:22 -0500

    Install Java

--- machine_setup/WSL/reset_wsl_clock.bat
+++ machine_setup/WSL/reset_wsl_clock.bat
@@ -1,7 +1,7 @@
 %REM Trigger this to run on workstation unlock (in Windows Task Scheduler)
 %REM Make sure to target the right WSL distro (Run `wsl --list` first)
+%REM Should *not* require WSL restart
 
 wsl --distribution Ubuntu-22.04 ^
     --user root ^
     --exec sudo hwclock -s
-
