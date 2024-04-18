958ad96b23e5360f1c33c8a9d21a732ca27acd86
tloeber <thomas.loeber73@gmail.com>
2024-04-18 08:42:07 -0400

    Close Docker Desktop before restarting WSL. *Force*-close VSCode.

--- machine_setup/WSL/restart_wsl.bat
+++ machine_setup/WSL/restart_wsl.bat
@@ -1,10 +1,14 @@
-@REM Restart WSL (have to close VSCode first)
-taskkill /IM Code.exe
-echo It is expected that this cannot terminate all VSCode processes because we are not using /F
+@REM Close VSCode and Docker Desktop first
+taskkill /F /IM "Docker Desktop"
+taskkill  /F /IM Code.exe
 
 @REM Note that WSL starts up again automatically
 wsl --shutdown
 
-@REM Start VSCode window again. (Opens last window.)
+@REM Start Docker Desktop
+timeout /t 5
+start "Docker Desktop"
+
+@REM Start VSCode window again. Opens last window(s).
 timeout /t 5
 code
