bdd49122e1f8ed84703cf7928470aff89a7e7be2
tloeber <thomas.loeber73@gmail.com>
2024-09-15 09:32:26 -0400

    cleanup

--- shell-dotfiles/hardlink-dotfiles.sh
+++ shell-dotfiles/hardlink-dotfiles.sh
@@ -6,7 +6,7 @@ mv ~/.bashrc ~/.bashrc.bak
 mv ~/.bash_aliases ~/.bash_aliases.bak || echo "No existing bash_aliases found, skipping backup"
 
 # Hard-link dotfiles from repo
-ln profile ~/.profile
-ln bash_profile ~/.bash_profile
-ln bashrc ~/.bashrc
-ln bash_aliases ~/.bash_aliases
+ln .profile ~/.profile
+ln .bash_profile ~/.bash_profile
+ln .bashrc ~/.bashrc
+ln .bash_aliases ~/.bash_aliases
